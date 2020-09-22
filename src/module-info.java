b5291f963d4d64f4a2e4ab920bf86b41d8f83f8f
olepbr <olepbr@gmail.com>
2020-09-22 22:46:49 +0000

    Redadded module-info

---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+module ovinger {
+    requires javafx.base;
+	requires javafx.controls;
+	requires javafx.fxml;
+    requires javafx.graphics;
+    requires junit;
+    requires org.eclipse.xtext.xbase.lib;
+}
